828fff2d5e9e47f20e65ee6ead8baccb4a1f9e30
Roche Amor <rocheamor.gador@gmail.com>
2023-07-13 22:26:38 +1000

    added project descriptions

---
+++ script.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', function() {
+//     let navbar = document.getElementById('navbar');
+//     if (window.scrollY > 0) {
+//       navbar.classList.add('sticky');
+//     } else {
+//       navbar.classList.remove('sticky');
+//     }
+//   });
